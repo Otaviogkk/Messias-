204a43d76a30a9b6f3d7c6a4753b2509e9cf1285
Otaviogkk <joseotaviolucianosousachagas@gmail.com>
2026-05-14 07:59:07 -0300

    Add files via upload

---
+++ ContaPoupanca.java
@@ -0,0 +1,9 @@
+public class ContaPoupanca extends Conta{
+   private double Rendimento;
+   public double getRendimento(){
+    return this.Rendimento;
+   }
+  public void setRendimento(double Rendimento){
+    this.Rendimento = Rendimento;
+  }
+}
